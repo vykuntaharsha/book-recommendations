324f55ddcdab01ec13d8172f2478c643a80beec4
Harsha Vykunta <harshavardhan.biit@gmail.com>
2018-02-24 01:46:30 -0800

    rearraged files, merged book-details and added footer

--- src/app-server.js
+++ src/app-server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
-app.use(express.static('src/public'));
+app.use(express.static('src/ui'));
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
